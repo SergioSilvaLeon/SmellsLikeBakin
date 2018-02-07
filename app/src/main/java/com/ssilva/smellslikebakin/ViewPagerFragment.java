c95475ff0b12f001557eb966567908c19f8d4b48
Sergio Silva <ssilva@nearsoft.com>
2018-02-07 01:45:20 -0700

    - Pass Arguments to a fragment using a bundle.

--- app/src/main/java/com/ssilva/smellslikebakin/ViewPagerFragment.java
+++ app/src/main/java/com/ssilva/smellslikebakin/ViewPagerFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 /**
  * Created by sergiosilva on 2/7/18.
@@ -13,10 +14,14 @@ import android.view.ViewGroup;
 
 public class ViewPagerFragment extends Fragment {
 
+    public static final String KEY_RECIPE_INDEX = "KEY_RECIPE_INDEX";
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        int index = getArguments().getInt(KEY_RECIPE_INDEX);
         View view = inflater.inflate(R.layout.fragment_viewpager, container, false);
+        Toast.makeText(getActivity(), Recipes.names[index], Toast.LENGTH_SHORT).show();
         return view;
     }
 }
