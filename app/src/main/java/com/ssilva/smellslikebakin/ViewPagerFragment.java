4b24fcc93571a18c2328dcf340b97c379bfd2796
Sergio Silva <ssilva@nearsoft.com>
2018-02-07 01:34:12 -0700

    - Handle when a fragment is opened and closed

---
+++ app/src/main/java/com/ssilva/smellslikebakin/ViewPagerFragment.java
@@ -0,0 +1,22 @@
+package com.ssilva.smellslikebakin;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by sergiosilva on 2/7/18.
+ */
+
+public class ViewPagerFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_viewpager, container, false);
+        return view;
+    }
+}
