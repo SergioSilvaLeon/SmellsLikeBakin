e5044ba93f4f36495e0a16e81e6437f70842fa9a
Sergio Silva <ssilva@nearsoft.com>
2018-02-08 01:09:37 -0700

    - Viewpager and tablayout

--- app/src/main/java/com/ssilva/smellslikebakin/ViewPagerFragment.java
+++ app/src/main/java/com/ssilva/smellslikebakin/ViewPagerFragment.java
@@ -2,7 +2,10 @@ package com.ssilva.smellslikebakin;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +25,31 @@ public class ViewPagerFragment extends Fragment {
         int index = getArguments().getInt(KEY_RECIPE_INDEX);
         View view = inflater.inflate(R.layout.fragment_viewpager, container, false);
         getActivity().setTitle(Recipes.names[index]);
+
+        final IngredientesFragment ingredientesFragment = new IngredientesFragment();
+        final DirectionsFragment directionsFragment = new DirectionsFragment();
+
+        ViewPager viewPager = view.findViewById(R.id.viewPager);
+        viewPager.setAdapter(new FragmentPagerAdapter(getChildFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return position == 0 ? ingredientesFragment : directionsFragment;
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return position == 0 ? "Ingridients" : "Directions";
+            }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+        });
+
+        TabLayout tabLayout = view.findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+
         return view;
     }
 
